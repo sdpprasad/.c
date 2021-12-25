208eae37c97ec5f29a608f6849504e5155b7dea9
sdpprasad <96641595+sdpprasad@users.noreply.github.com>
2021-12-25 09:59:38 +0530

    Add files via upload

---
+++ sdp.c
@@ -0,0 +1,6 @@
+/*a c program to display f block*/
+#include<stdio.h>
+int main()
+{printf("#####\n#\n####\n#\n#\n#");
+return 0;
+}
---
+++ sdp12.c
@@ -0,0 +1,11 @@
+/* a c program  to roots of a quadratic equation*/
+#include<stdio.h>
+int main()
+{
+int l=7,b=5,a,p;
+a=l*b;
+p=2*(l+b);
+printf("area and perimeter of rectangle is %d  %d",a,p);
+return 0;
+}
+
